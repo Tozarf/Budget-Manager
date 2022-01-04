4e82f49fb832478d57e007054dc7e4a3ecc97aa4
Tozarf <fazatojuni@gmail.com>
2022-01-03 19:42:35 -0600

    testing vscode

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -24,7 +24,6 @@ const iconsManager = {
     Health: HealthIcon,
     Suscriptions: SuscriptionsIcon,
 };
-//testing
 export const Expense = ({ expense, setExpenseEdit }) => {
     const { name, quantity, category, id, date } = expense;
     const leadingActions = () => (
